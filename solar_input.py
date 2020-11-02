e721970e612e938bfb30c7114f396dc10b4384fe
Grib <firsov.va@phystech.edu>
2020-11-02 10:02:49 +0300

    &

--- solar_input.py
+++ solar_input.py
@@ -43,7 +43,7 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
+    #pam-pam
     pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
